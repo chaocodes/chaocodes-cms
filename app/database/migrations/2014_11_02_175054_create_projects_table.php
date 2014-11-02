a179db25fd060c83056512f2874016780e406dcd
Chao Lai <chao.lai@uconn.edu>
2014-11-02 13:12:56 -0500

    Projects migration and model

---
+++ app/database/migrations/2014_11_02_175054_create_projects_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration 
+{
+	public function up()
+	{
+		Schema::create('projects', function($table)
+		{
+			$table->increments('id');
+			$table->string('name', 30);
+			$table->string('url', 30);
+			$table->string('image', 30);
+			$table->text('description');
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+}
