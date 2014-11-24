68bad195c56da602c58789616eb5fd789361bd42
Chao Lai <chao.lai@uconn.edu>
2014-11-24 12:36:44 -0500

    Created and routed 404 page not found error page

---
+++ app/views/errors/default.blade.php
@@ -0,0 +1,8 @@
+@extends('pages.master')
+
+@section('content')
+<div class="content">
+	<h1>An error has occurred!</h1>
+	<p>{{HTML::link('/', 'Click here to return to the home page.')}}</p>
+</div>
+@stop
